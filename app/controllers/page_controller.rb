b39ac39ed9eed11f6e9c46c727e1479d5628567e
Adam Markowski <adam.markowski.uj@gmail.com>
2017-09-09 16:06:30 +0200

    Updated layout

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,7 +2,7 @@ class PageController < ApplicationController
 
   def show
     @page = Page.find_by_page(params[:page])
-    fresh_when(etag: @page, public: true)
+    fresh_when(etag: @page.cache_key, public: true)
   end
 
 end
